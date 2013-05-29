d268d924a7a1fc9f616f3cb0d93dfb83f9475ce9
unknown <Tome.Hristovski@tome_h.interworks.local>
2013-05-29 11:55:14 +0200

    Added validation

--- spec/requests/static_pages_title.rb
+++ spec/requests/static_pages_title.rb
@@ -1,16 +1,49 @@
 require 'spec_helper'
 
-  describe "should have the title" do
+  describe "Page should have the" do
     
-    it "should have the title 'Home'" do
+    # Check the title in the Home page
+    it "'Title Home'" do
       visit '/pages/home'
-      expect(page).to have_selector('title', :content =>'Ruby on Rails Tutorial Sample App | Home')
-    end
-
-    it "should have the title 'About'" do
-      visit '/pages/about'
-      expect(page).to have_selector('title', :content =>'Ruby on Rails Tutorial Sample App | About')
+    # expect(page).to have_selector('title', :text =>'Ruby on Rails Tutorial Sample App | Home')
+      page.should have_selector('title', :text =>/\bRuby on Rails Tutorial Sample App\s\W\s\b#{@title}/)
+      puts "Test on the Home page passed successfully"
     end
     
-end
+    # Check the h1 in the Home page
+    it "h1 'Home'" do
+      visit '/pages/home'
+      page.should have_selector('h1', :text => 'Home')
+      puts "Test on the Home page passed successfully"
+    end
+    
+    # Check the title in the About page
+    it "Title 'About'" do
+      visit '/pages/about'
+    # expect(page).to have_selector('title', :text =>'Ruby on Rails Tutorial Sample App | About')
+      page.should have_selector('title', :text =>/\bRuby on Rails Tutorial Sample App\s\W\s\b#{@title}/)
+      puts "Test on the About page passed successfully"
+    end
+    
+    # Check the title in the Contact page and also validate the title  
+    it "Title 'Contact'" do
+      visit '/pages/contact'
+      page.should have_selector('title', :text =>/\bRuby on Rails Tutorial Sample App\s\W\s\b#{@title}/)
+      puts "Test on the Contact page passed successfully"
+    end
+    
+    # Check the title in the Help page   
+    it "Title 'Help'" do
+      visit '/pages/help'
+      page.should have_selector('title', :text =>/\bRuby on Rails Tutorial Sample App\s\W\s\b#{@title}/)     
+      puts "Test on the Help page passed successfully"
+    end
 
+#    Should_not have Title Home
+#    it "'Home'" do
+#      visit '/pages/home'
+#      page.should_not have_selector('title', :text => '| Home')
+#      puts "Test passed successfully"
+#    end
+     
+end
