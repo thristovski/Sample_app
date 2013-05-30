449aaf149df4789162aec792cc072a278595a8dc
unknown <Tome.Hristovski@tome_h.interworks.local>
2013-05-30 13:06:00 +0200

    Updated tests

--- spec/requests/static_pages_title.rb
+++ spec/requests/static_pages_title.rb
@@ -1,56 +1,42 @@
 require 'spec_helper'
 
-  describe "Page should have the" do
-    
-    # Check the title in the Home page
-    it "'Title Home'" do
-      visit root_path
-    # expect(page).to have_selector('title', :text =>'Ruby on Rails Tutorial Sample App | Home')
-      page.should have_selector('title', :text =>/\bRuby on Rails Tutorial Sample App\s\W\s\b#{@title}/)
-      puts "Test on the Home page passed successfully"
-    end
-    
-    # Check the h1 in the Home page
-    it "h1 'Home'" do
-      visit root_path
-      page.should have_selector('h1', :text => 'Home')
-      puts "Test on the Home page passed successfully"
-    end
-    
-    # Check the title in the About page
-    it "Title 'About'" do
-      visit about_path
-    # expect(page).to have_selector('title', :text =>'Ruby on Rails Tutorial Sample App | About')
-      page.should have_selector('title', :text =>/\bRuby on Rails Tutorial Sample App\s\W\s\b#{@title}/)
-      puts "Test on the About page passed successfully"
-    end
-    
-    # Check the title in the Contact page and also validate the title  
-    it "Title 'Contact'" do
-      visit contact_path
-      page.should have_selector('title', :text =>/\bRuby on Rails Tutorial Sample App\s\W\s\b#{@title}/)
-      puts "Test on the Contact page passed successfully"
-    end
-    
-    # Check the h1 in the Contact page
-    it "h1 'Home'" do
-      visit contact_path
-      page.should have_selector('h1', :text => 'Contact')
-      puts "Test on the Contact page passed successfully"
-    end
-    
-    # Check the title in the Help page   
-    it "Title 'Help'" do
-      visit help_path
-      page.should have_selector('title', :text =>/\bRuby on Rails Tutorial Sample App\s\W\s\b#{@title}/)     
-      puts "Test on the Help page passed successfully"
-    end
+describe "Static pages" do
 
+  subject { page }
+
+  describe "Home" do
+    before { visit root_path }
+    it { should have_selector('title', :text =>/\bRuby on Rails Tutorial Sample App\s\W\s\b#{@title}/) }
+    it { should have_selector('h1', :text => 'Home') }
+    puts "Test on the Home page passed successfully"
+  end
+
+   describe "Help page" do
+    before { visit help_path }
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
+    puts "Test on the Help page passed successfully"
+  end
+
+  describe "About page" do
+    before { visit about_path }
+    it { should have_selector('h1', text: 'About') }
+    it { should have_selector('title', text: full_title('About Us')) }
+    puts "Test on the About page passed successfully"
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
+    puts "Test on the Contact page passed successfully"
+  end
+  
 #    Should_not have Title Home
 #    it "'Home'" do
 #      visit '/pages/home'
 #      page.should_not have_selector('title', :text => '| Home')
 #      puts "Test passed successfully"
 #    end
-     
+
 end
