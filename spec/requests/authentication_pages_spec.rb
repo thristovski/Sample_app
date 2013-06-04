c38729aaeb9191c6384e94102941dca5de007dc0
unknown <Tome.Hristovski@tome_h.interworks.local>
2013-06-04 14:54:57 +0200

    Finish user edit, update, index, and destroy actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -11,6 +11,26 @@ describe "Authentication" do
     it { should have_selector('title', text: 'Sign in') }
   end
 
+  describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+    end
+  end
+
   describe "signin" do
     before { visit signin_path }
 
